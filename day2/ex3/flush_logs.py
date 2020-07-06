ba7f24c0d8ea57c6f5a9f70e576ed3ba5b5de3f5
psyoblade <park.suhyuk@gmail.com>
2020-07-06 23:52:11 +0900

    [개발] 시스템 로그를 테일링 하면서 표준 출력으로 전달

---
+++ day2/ex3/flush_logs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys, time, os, shutil
+
+# 1. read apache_logs flush every 100 lines until 1000 lines
+# 2.   every 1000 lines file close & rename file with seq
+# 3. create new accesslogs and goto 1.
+
+def readlines(fp, num_of_lines):
+    lines = ""
+    for line in fp:
+        lines += line
+        num_of_lines = num_of_lines - 1
+        if num_of_lines == 0:
+            break
+    return lines
+
+
+fr = open("apache_logs", "r")
+for x in range(0, 10):
+    fw = open("source/accesslogs", "w+")
+    for y in range(0, 10):
+        lines = readlines(fr, 100)
+        fw.write(lines)
+        fw.flush()
+        time.sleep(0.1)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+    fw.close()
+    print("file flushed ... sleep 10 secs")
+    time.sleep(10)
+    # os.rename("source/accesslogs", "source/accesslogs.%d" % x)
+    shutil.move("source/accesslogs", "source/accesslogs.%d" % x)
+    print("renamed accesslogs.%d" % x)
+fr.close()
