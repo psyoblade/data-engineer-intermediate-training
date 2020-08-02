edc97fc6fb48832e9b34e4d58f423aa21895f20a
SuHyuk Park <park.suhyuk@gmail.com>
2020-08-02 13:34:34 +0900

    [수정] 필요없는 파일 삭제

---
+++ basic/work/php/index.php
@@ -0,0 +1,13 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, World!";
+        ?>
+    </body>
+</html>
