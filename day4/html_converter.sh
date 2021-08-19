59a517042dc16a52b56c0f38a1d3e93906217595
psyoblade <park.suhyuk@gmail.com>
2021-08-19 22:47:59 +0900

    [수정] 노트북 html 변환결과

--- day4/html_converter.sh
+++ day4/html_converter.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-rm notebooks/*.html
-docker-compose exec notebook sh -c "jupyter nbconvert work/*.ipynb --to html"
+lectures="core delta stream troubleshoot"
+for lecture in $lectures; do
+    notebook="lgde-spark-$lecture"
+    notepath="notebooks/$notebook"
+    echo "processing notebook('$notepath')"
+    rm $notepath/*.html
+    docker-compose exec notebook sh -c "jupyter nbconvert work/$notebook/*.ipynb --to html"
+    sleep 5
+done
